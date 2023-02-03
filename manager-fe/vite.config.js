d23f87a93d17089a25672f9d7fb9d29e26caaa2e
ZiSummer <838385736@qq.com>
2023-02-03 16:40:58 +0800

    Update vite.config.js
    修改文件格式

--- manager-fe/vite.config.js
+++ manager-fe/vite.config.js
@@ -3,15 +3,15 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-      host: 'localhost',
-      port: 8080,
-      // 将所有以 '/api' 开头的请求代理到 http://localhost:3000
-      proxy: {
-          "/api": {
-              target: "http://localhost:3000"
-          }
-      }
-  },
-  plugins: [vue()],
+    server: {
+        host: 'localhost',
+        port: 8080,
+        // 将所有以 '/api' 开头的请求代理到 http://localhost:3000
+        proxy: {
+            "/api": {
+                target: "http://localhost:3000"
+            }
+        }
+    },
+    plugins: [vue()],
 })
